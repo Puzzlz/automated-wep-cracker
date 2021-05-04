4f003728afcf08fad1a4fc8a40be03016f86b8d4
Tyson Steele <tysonsteele77@homail.com>
2021-05-04 16:53:23 -0400

    Added license

--- sniffer.py
+++ sniffer.py
@@ -1,3 +1,17 @@
+# Automatic wep cracker
+# Copyright (C) 2021  Tyson Steele, Neilesh Chander, Ryan Kane
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+
 # NOTE
 # 50% chance IV repeats after 5000 packets
 
